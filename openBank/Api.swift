0204069e999abb66f452f56758a2600bd70e2b7f
cristobalramos93 <cristora@ucm.es>
2022-03-07 11:12:42 +0100

    added .json

--- openBank/Api.swift
+++ openBank/Api.swift
@@ -8,9 +8,11 @@
 import Foundation
 class API {
     let url: URL
+    
     init(url: URL) {
         self.url = url
     }
+    
     func performRequest(_ completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
         URLSession.shared.dataTask(with: self.url, completionHandler: completion).resume()
     }
--- openBank/SceneDelegate.swift
+++ openBank/SceneDelegate.swift
@@ -11,24 +11,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        // 1
-            guard let windowScene = (scene as? UIWindowScene) else { return }
-            
-            // 2
-            window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-            window?.windowScene = windowScene
-
-            // 3
-            window?.rootViewController = CharactersListViewController()
-            window?.makeKeyAndVisible()
-        
-        
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = CharactersListViewController()
+        window?.makeKeyAndVisible()
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
